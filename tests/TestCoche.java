b882ae1cc19861d8d9f3941d8cbb222e21333876
Adrimacdu <116901830+Adrimacdu@users.noreply.github.com>
2023-04-02 18:16:33 +0200

    Update metodos Coche.java

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -11,7 +11,7 @@ public class TestCoche {
     @Test
     public void test_al_acelerar_un_coche_su_velocidad_aumenta_AdrianMaciaDuarte(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerarAdrianMac(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
@@ -19,7 +19,7 @@ public class TestCoche {
     public void test_al_decelerar_un_coche_su_velocidad_disminuye_AdrianMaciaDuarte(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerarAdrianMac(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
@@ -27,7 +27,7 @@ public class TestCoche {
     public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_AdrianMaciaDuarte(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerarAdrianMac(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
